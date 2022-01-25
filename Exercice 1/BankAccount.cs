aefda909aa88639c6e27ed09e197b2a0cac2ef72
RomRav <romain.ravault@gmail.com>
2022-01-25 11:04:35 +0100

    Exercice de modelisation et création des classes

---
+++ Exercice 1/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_1
+{
+    internal class BankAccount
+    {
+        public string titulaire { get; set; }
+        public double balance { get; set; }
+        public  string devise { get; set; }
+
+        public BankAccount(string titulaire, double balance, string devise)
+        {
+            titulaire = titulaire;
+            balance = balance;  
+            devise = devise;
+        }
+
+        /// <summary>
+        /// Méthode de crédit du compte bancaire
+        /// </summary>
+        /// <param name="amount"></param>
+        public void credit(double amount)
+        {
+            balance += amount;
+        }
+        /// <summary>
+        /// Méthode de débit du compte bancaire
+        /// </summary>
+        /// <param name="amount"></param>
+        public void debit(double amount)
+        {
+            balance -= amount;
+        }
+
+    }
+}
---
+++ Exercice 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double inputBalance;
+            Double inputAmount;
+            BankAccount CompteLaManu = new BankAccount("", 0, "");
+            Console.WriteLine("Merci de saisir le nom du titulaire du compte : ");
+            CompteLaManu.titulaire = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Merci de saisir le montant du solde du compte : ");
+                if (Double.TryParse(Console.ReadLine(), out inputBalance))
+                {
+                    CompteLaManu.balance = inputBalance;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Saisie incorrecte!");
+                }          
+            }
+            Console.WriteLine("Merci de saisir la devise du compte : ");
+            CompteLaManu.devise = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Merci de saisir le montant à créditer : ");
+                if (Double.TryParse(Console.ReadLine(), out inputAmount))
+                {
+                    CompteLaManu.credit(inputAmount);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Saisie incorrecte!");
+                }
+            }
+            Console.WriteLine($"Le solde du compte de {CompteLaManu.titulaire} est de {CompteLaManu.balance} {CompteLaManu.devise}.");
+            Console.ReadLine();
+        }
+    }
+}
